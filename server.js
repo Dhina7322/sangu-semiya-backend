3015a14f0609ddc117eff1b85501ccb7ca4c0bed
Dhina7322 <dhina7322@gmail.com>
2026-04-02 15:40:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use('/uploads', express.static('uploads'));
 
 // Routes
